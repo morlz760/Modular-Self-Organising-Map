c1a6b571a361b31566647ce4438d138224977564
morlz760 <morley.pemberton@gmail.com>
2022-06-12 12:18:14 +1000

    finalising scripts

--- mdsom_functions.py
+++ mdsom_functions.py
@@ -40,11 +40,11 @@ def unnest_data(data):
         return(unnested_data)
 
 # Create and train a SOM
-def create_train_som(data, n_features, grid_size = [8,8], convolutional_layer = False):
+def create_train_som(data, n_features, grid_size = [8,8], convolutional_layer = False, sigma = 1.5, learning_rate=0.5):
     # Create SOM dimensions
     if convolutional_layer:
         data = unnest_data(data)
-        n_features = n_features*2
+        n_features = n_features
     else:
         data = data
     # Create SOM dimensions
@@ -53,7 +53,7 @@ def create_train_som(data, n_features, grid_size = [8,8], convolutional_layer = 
     y = grid_size[1]
     print("Som Neurons", x*y)
     #Create and train SOM
-    som = MiniSom(x, y, n_features, sigma=0.3, learning_rate=0.5) # initialization of x X y som
+    som = MiniSom(x, y, n_features, sigma=sigma, learning_rate=learning_rate) # initialization of x X y som
     som.random_weights_init(data)
     som.train_random(data, 10000, verbose=False) # training with 100 iterations
     return som
@@ -77,7 +77,7 @@ def data_vectorisation(data):
     else:
         return(data_values_array)
 
-def train_som_layer(data, feature_collections, grid_size = [8,8], convolutional_layer = False):
+def train_som_layer(data, feature_collections, grid_size = [9,9], convolutional_layer = False):
     # create a dictionary to store the trained SOM
     trained_soms = {}
     # For each feature in the data set we will train a SOM. I intend to update this so that we can pass this function a list of combos we want to 
@@ -104,7 +104,8 @@ def train_som_layer(data, feature_collections, grid_size = [8,8], convolutional_
 # This means that you'll use this function in the creation of the MDSOM and then also in the testing process. The Traind_SOMS layer is the 
 # static element, the convolutional layer is desitned to change depending on the data that's fed into it. So in the testing process you'll
 # have to create a new convolutional layer 
-def create_convolution_layer(data, trained_soms, feature_collections, normalise=False):
+# This keeps the x y coordiants and the weight
+def create_sampling_layer_xyw(data, trained_soms, feature_collections, normalise=False):
     # Create empty dataframe to store the winning nodes from our trained SOM's
     dataframe = pd.DataFrame()
     # loop through each of the featuresets that have been used to build the SOM's to extarct the output from these values
@@ -138,12 +139,14 @@ def create_convolution_layer(data, trained_soms, feature_collections, normalise=
             node_distance = distance_map[winning_pos]
             # We now convert this coordinant into a numerical value so we can feed it to our next layer, to to do this properly
             # we need the y value of the SOM as that helps us convery coords to a single didget.
-            node_value = convert_coordinants(winning_pos, som_y_dim)
-            winning_node_value.append(node_value)
+            winning_pos = pd.array(winning_pos, dtype=np.int32)
+            # print(winning_pos)
+            winning_node_value.append(winning_pos)
+            # print(winning_node_value)
             winning_node_distance.append(node_distance)
         # Here we evaluate if we want to normalise our data or not. In this example we are normalising column wise. 
         if normalise:
-            winning_node_value_array = pd.array(winning_node_value, dtype=np.int32).reshape(-1,1)
+            # print(winning_node_value_array)
             winning_node_value_normalised = preprocessing.normalize(winning_node_value_array, axis = 0).flatten()
             winning_node_distance_array = pd.array(winning_node_distance, dtype=np.float32).reshape(-1,1)
             winning_node_distance_normalised = preprocessing.normalize(winning_node_distance_array, axis = 0).flatten()
@@ -151,39 +154,8 @@ def create_convolution_layer(data, trained_soms, feature_collections, normalise=
             dataframe[observation_key] = winning_nodes
         else:
             winning_nodes = np.array(list(zip(winning_node_value, winning_node_distance))).tolist()
-            dataframe[observation_key] = winning_nodes
-    return(dataframe)
-
-def create_convolution_layer_only_winning_som(data, trained_soms, feature_collections, normalise=False):
-    dataframe = pd.DataFrame()
-    # loop through each of the featuresets that have been used to build the SOM's to extarct the output from these values
-    # that will be used to create the convolv layer.
-    for feature_set in feature_collections:
-        # So for each feature set extarct the corrosponding data from the training data.
-        print("Creating convolutional layer for: ", feature_set)
-        train_value = data[feature_set]
-        if len(train_value.dtypes.unique()) > 1:
-            print("Your feature sets have incompatable data formats")
-        if (train_value.dtypes == 'O').all():
-            train_value_array = unnest_data(train_value)
-        else:
-            train_value_array = train_value.values
-        observation_key = "".join(map(str,feature_set))
-        som = trained_soms.get(observation_key)[0]
-        # Extract the y dimension of the given SOM.
-        som_y_dim = max(som._neigy) + 1
-        print("Y som dims: ",som_y_dim)
-        winning_node_value = []
-        # loop through the array of observations and extract the winning node and its distance for the given observation.
-        for observation in train_value_array:
-            winning_pos = som.winner(observation)
-            node_value = convert_coordinants(winning_pos, som_y_dim)
-            winning_node_value.append(node_value)
-        if normalise:
-            winning_node_distance_normalised = preprocessing.normalize(node_value, axis = 0).flatten()
-            dataframe[observation_key] = winning_node_distance_normalised
-        else:
-            dataframe[observation_key] = winning_node_value
+            winning_node_array = np.array([np.hstack(i) for i in winning_nodes]).tolist()
+            dataframe[observation_key] = winning_node_array
     return(dataframe)
 
 # This function is used to evaluate the node purity of the output. One way to measure the effecacy of the algo.
@@ -214,7 +186,7 @@ def evaluate_purity(som, data, targets, convolutional_layer=False):
     # Calculate the overall purity for the layer
     node_purity = winmapDFT['node_purity'].mean(axis=0)
     weighted_node_purity = winmapDFT['weighted_node_purity'].sum(axis=0)
-    return(winmapDFT)
+    return(weighted_node_purity)
 
 def label_output(som, data, targets, final_convolution = pd.DataFrame(), convolutional_layer = False, original = True):
     # Get the winning values
@@ -290,7 +262,7 @@ def som_map_plot(data, labels, som, convolutional_layer = False):
     plt.update_traces(marker_size=25)
     return(plt)
 
-def plot_som_win_map(data, labels, som, title = "Som Win Map", sampled_layer = False):
+def plot_som_win_map(data, labels, som, title = "Som Win Map", sampled_layer = False, simple = True):
     import plotly.express as px
     if sampled_layer:
         data_values = unnest_data(data)
@@ -302,8 +274,12 @@ def plot_som_win_map(data, labels, som, title = "Som Win Map", sampled_layer = F
     winmapDFT = pd.DataFrame(winmap).T
     winmapDFT['class'] = winmapDFT.apply(lambda x: winmapDFT.columns[x.argmax()], axis = 1).astype(str) 
     winmapDFT = winmapDFT.reset_index()
-    winmapDFT["max_val_node"] = winmapDFT[[1,2,3]].max(axis=1)
-    winmapDFT["total_obs_node"] = winmapDFT[[1,2,3]].sum(axis=1)
+    if simple:
+        winmapDFT["max_val_node"] = winmapDFT[[1,2,3]].max(axis=1)
+        winmapDFT["total_obs_node"] = winmapDFT[[1,2,3]].sum(axis=1)
+    else:
+        winmapDFT["max_val_node"] = winmapDFT[[5,6,7,4,8,3]].max(axis=1)
+        winmapDFT["total_obs_node"] = winmapDFT[[5,6,7,4,8,3]].sum(axis=1)
     winmapDFT["node_purity"] = winmapDFT["max_val_node"] / winmapDFT["total_obs_node"]
     # plot the data
     fig = px.scatter(winmapDFT, x="level_0", y="level_1", color="class", size="node_purity")
@@ -342,7 +318,5 @@ def plot_som_win_map(data, labels, som, title = "Som Win Map", sampled_layer = F
             bgcolor='rgba(255, 255, 255, 0)',
             bordercolor='rgba(255, 255, 255, 0)'
         )
-        # paper_bgcolor='rgba(0,0,0,0)',
-        # plot_bgcolor='rgba(0,0,0,0)'
     )
     fig.show()
